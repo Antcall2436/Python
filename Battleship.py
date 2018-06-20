2c57e7f5663cb3dd779140ddd3f64c1ec43026d7
Shresth Gupta <shresthgupta054@gmail.com>
2018-06-20 18:16:04 +0530

    Add files via upload
    Battleship Game: Guess the location of the ship. You have 4 turns to guess it right

--- Battleship.py
+++ Battleship.py
@@ -19,8 +19,8 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+#print ship_row
+#print ship_col
 
 # Everything from here on should go in your for loop!
 # Be sure to indent four spaces!
@@ -30,6 +30,7 @@ for turn in range(4):
   guess_col = int(raw_input("Guess Col: "))
   if guess_row == ship_row and guess_col == ship_col:
     print "Congratulations! You sunk my battleship!"
+    break
   else:
     if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
       print "Turn: %s" % (turn+1)
@@ -40,4 +41,6 @@ for turn in range(4):
       print "You missed my battleship!"
       board[guess_row][guess_col] = "X"
       # Print (turn + 1) here!
+    if turn == 3:
+      print "Game Over"
     print_board(board)
